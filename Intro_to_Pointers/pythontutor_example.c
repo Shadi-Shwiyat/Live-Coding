c4e81aebfdfb6a16ef3a3f2d659058cdde0355fa
Shadi-Shwiyat <raz611103@gmail.com>
2023-05-26 07:42:42 -0700

    Added a pytutor example to show character pointers and how they are used

---
+++ Intro_to_Pointers/pythontutor_example.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * main - program prints a string to
+ * the console using each individual
+ * character with putchar and a loop
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int len = 0;
+	char *str = "Hello World";
+	char arr_str[] = "Holberton";
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	i = 0;
+
+	while (arr_str[len] != '\0')
+	{
+		len++;
+	}
+
+	while (i < len)
+	{
+		_putchar(arr_str[i]);
+		i++;
+	}
+	_putchar('\n');
+
+}
